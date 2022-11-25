3512d5d110674179918f816239ac4601cfd48475
Alexis <alexisjrojas@hotmail.es>
2022-11-25 13:10:59 -0300

    Carrito emote agregado

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CartWidget({ color }) {
+  return (
+    <a href="#" style={{ color: color }}>
+      <li>🛒</li>
+    </a>
+  );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { Link, useParams } from "react-router-dom";
+import CartWidget from "./CartWidget";
 function NavBar() {
   return (
     <ConNav>
-      <Link to="/">Inicio</Link>
-      <Link to="/category/electronics">Electronica</Link>
-      <Link to="/category/jewelery">Jewelery</Link>
-      <Link to="/category/women's clothing">Ropa de Mujeres</Link>
-      <Link
-        to="/category/men's clothing
+      <div>
+        <Link to="/">Inicio</Link>
+        <Link to="/category/electronics">Electronica</Link>
+        <Link to="/category/jewelery">Jewelery</Link>
+        <Link to="/category/women's clothing">Ropa de Mujeres</Link>
+        <Link
+          to="/category/men's clothing
 "
-      >
-        Ropa de Hombre
-      </Link>
+        >
+          Ropa de Hombre
+        </Link>
+      </div>
+      <CartWidget />
     </ConNav>
   );
 }
@@ -28,5 +32,15 @@ const ConNav = styled.header`
   gap: 0.5em;
   color: white;
   margin-bottom: 5px;
+  div {
+    width: 100%;
+    a {
+      margin: 5px;
+    }
+  }
+  li {
+    list-style: none;
+    margin-right: 2px;
+  }
 `;
 export default NavBar;
